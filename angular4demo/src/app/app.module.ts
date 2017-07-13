ec12de7db82696c0147ee3cf32749bb61be299b1
kostamas <kosta.masliansky@gmail.com>
2017-07-13 18:10:48 +0300

    save

--- angular4demo/src/app/app.module.ts
+++ angular4demo/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MdTableModule, MdTabsModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { SimpleComponentComponent } from './simple-component/simple-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SimpleComponentComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MdTableModule,
+    MdTabsModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
