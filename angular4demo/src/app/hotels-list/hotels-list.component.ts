c28114725f931f51e9bcaec5b2fc13cb9f4d69fb
kostamas <kosta.masliansky@gmail.com>
2017-07-19 19:17:27 +0300

    add routing

--- angular4demo/src/app/hotels-list/hotels-list.component.ts
+++ angular4demo/src/app/hotels-list/hotels-list.component.ts
@@ -4,7 +4,6 @@ import "rxjs/add/operator/pluck";
 import "rxjs/add/operator/map";
 import "rxjs/add/operator/filter";
 import 'rxjs/add/operator/switchMap';
-import * as Rx from "rxjs/Rx";
 import 'rxjs/add/observable/of';
 
 
